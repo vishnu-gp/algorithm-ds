1339aa2a101f5a3b8d4ece4b16813699fd635b07
Vishnu Gp <vishnu.aassk@gmail.com>
2021-08-02 15:53:33 +0530

    Added Flatten multi level linked list problem

---
+++ Excercises/LinkedLists/02_FlattenMultiLevelDoublyLinkedList.js
@@ -0,0 +1,38 @@
+/**
+ * // Definition for a Node.
+ * function Node(val,prev,next,child) {
+ *    this.val = val;
+ *    this.prev = prev;
+ *    this.next = next;
+ *    this.child = child;
+ * };
+ */
+
+/**
+ * @param {Node} head
+ * @return {Node}
+ */
+const flatten = function (head) {
+    let currentNode = head, level = 0
+    const tail = {}
+    while (currentNode) {
+        if (currentNode.child) {
+            tail[level] = currentNode.next
+            currentNode.next = currentNode.child
+            currentNode.child = null
+            currentNode.next.prev = currentNode
+            level++
+        }
+        if (!currentNode.next && level) {
+            currentNode.next = tail[--level]
+            if (currentNode.next)
+                currentNode.next.prev = currentNode
+            else
+                currentNode = currentNode.prev
+        }
+        currentNode = currentNode.next
+
+    }
+
+    return head
+};
